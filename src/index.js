bc66bf10daeee7b244e562951346213c87cbc453
nkped <nikrpedev@gmail.com>
2022-03-12 13:45:15 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+render(
+  <Router>
+    <App />
+  </Router>,
+  rootElement
+);
